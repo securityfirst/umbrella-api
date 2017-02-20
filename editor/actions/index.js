0717fb2c24c2665c2a92a9d5dec7dc0f7f921e29
Rok Biderman <rok.biderman@gmail.com>
2017-02-20 06:56:28 -0800

    Interceptor picking up cookie token

--- editor/actions/index.js
+++ editor/actions/index.js
@@ -1,15 +1,22 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
 import cookie from 'react-cookie';
-import { CATEGORIES,
+import { AUTH_USER,
+         ACTIVE_CAT,
+         TREE,
+         FETCH_USER,
          SEGMENTS,
          CHECK_ITEMS } from './types';
 
 const API_URL = 'http://127.0.0.1:8080/v2';
 
+axios.interceptors.request.use(function (config) {
+  config.headers['Authorization'] = 'Bearer ' + cookie.load('github-email');
+  return config;
+});
+
 export function errorHandler(dispatch, error, type) {
   let errorMessage = '';
-
   if(error.data.error) {
     errorMessage = error.data.error;
   } else if(error.data) {
@@ -18,11 +25,12 @@ export function errorHandler(dispatch, error, type) {
     errorMessage = error;
   }
 
-  if(error.status === 401) {
+  if(error.status === 401 || error.status === 403) {
     dispatch({
       type: type,
       payload: 'You are not authorized to do this. Please login and try again.'
     });
+    browserHistory.push('/login');
   } else {
     dispatch({
       type: type,
@@ -32,15 +40,50 @@ export function errorHandler(dispatch, error, type) {
 }
 
 export function getRepos() {
-  console.log("get repos");
   return function(dispatch) {
     axios.get(`${API_URL}/api/tree`)
     .then(response => {
       dispatch({
-        type: CATEGORIES,
+        type: TREE,
         payload: response.data
       });
-      // console.log(response.data);
     })
+  }
+}
+
+export function getInfo() {
+  return function(dispatch) {
+    axios.get(`${API_URL}/`)
+    .then(response => {
+      dispatch({
+        type: FETCH_USER,
+        payload: response.data
+      });
+      dispatch({
+        type: AUTH_USER
+      });
+    })
+    .catch(function (error) {
+      return errorHandler(dispatch, error.response, UNAUTH_USER);
+    });
+  }
+}
+
+export function getCat(categoryName) {
+  return function(dispatch) {
+    if (typeof myVar == 'undefined') return
+    axios.get(`${API_URL}/repo`+categoryName)
+    .then(response => {
+      dispatch({
+        type: ACTIVE_CAT,
+        payload: response.data
+      });
+      dispatch({
+        type: AUTH_USER
+      });
+    })
+    .catch(function (error) {
+      return errorHandler(dispatch, error.response, UNAUTH_USER);
+    });
   }
 }
