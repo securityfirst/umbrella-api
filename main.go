8800c5ff03e2620ffe8e6a242f14341acc17c482
Alex Guerrieri <klaidliadon@gmail.com>
2017-12-14 17:47:21 +0000

    v3 api

--- main.go
+++ main.go
@@ -9,10 +9,14 @@ import (
 
 	cors "github.com/itsjamie/gin-cors"
 
-	"github.com/securityfirst/tent"
-	"github.com/securityfirst/tent/auth"
-	"github.com/securityfirst/tent/repo"
 	"github.com/securityfirst/umbrella-api/utils"
+	"gopkg.in/securityfirst/tent.v1"
+	"gopkg.in/securityfirst/tent.v1/auth"
+	"gopkg.in/securityfirst/tent.v1/repo"
+
+	tent2 "gopkg.in/securityfirst/tent.v2"
+	auth2 "gopkg.in/securityfirst/tent.v2/auth"
+	repo2 "gopkg.in/securityfirst/tent.v2/repo"
 
 	"github.com/gin-gonic/gin"
 	"github.com/tmilewski/goenv"
@@ -89,7 +93,7 @@ func main() {
 	}
 	v2 := r.Group("/v2")
 	{
-		r, err := repo.New("securityfirst", "tent-content")
+		r, err := repo.New("securityfirst", "tent-content", "master")
 		if err != nil {
 			log.Fatalf("Repo error: %s", err)
 		}
@@ -98,6 +102,28 @@ func main() {
 		v2.GET("/feed", um.getFeed)
 		// Autentication
 		o.Register(v2, conf)
+	}
+	v3 := r.Group("/v3")
+	{
+		r, err := repo2.New("securityfirst", "tent-content", "difficulty")
+		if err != nil {
+			log.Fatalf("Repo error: %s", err)
+		}
+		o := tent2.New(r)
+		// No authentication
+		v3.GET("/feed", um.getFeed)
+		// Autentication
+
+		o.Register(v3, auth2.Config{
+			Id:           conf.Id,
+			Secret:       conf.Secret,
+			OAuthHost:    conf.OAuthHost,
+			Host:         conf.Host,
+			RandomString: conf.RandomString,
+			Login:        auth2.HandleConf{conf.Login.Endpoint, conf.Login.Redirect},
+			Logout:       auth2.HandleConf{conf.Logout.Endpoint, conf.Logout.Redirect},
+			Callback:     auth2.HandleConf{conf.Callback.Endpoint, conf.Callback.Redirect},
+		})
 	}
 	r.Run(":8080")
 
